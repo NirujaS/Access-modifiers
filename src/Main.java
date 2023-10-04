ce73db2a1324bf3182f6141edf58bce164f298c1
Dell <40-bis-0022@kdu.ac.lk>
2023-10-04 16:58:14 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+
+        student.setName("Alice");
+        student.setAge(18);
+        student.setGrade('A');
+
+
+        student.displayInfo();
+    }
+}
